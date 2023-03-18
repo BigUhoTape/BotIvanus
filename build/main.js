7f4a6286f5ae7a1941b08decd8cfc1191ae6dd39
Ivan Vostrikov <vakann20@gmail.com>
2023-03-18 16:47:35 +0700

    init commit

--- build/main.js
+++
@@ -1,5 +0,0 @@
-import { analytics } from './modules/analytics.js';
-const message = 'Hello node';
-console.log(message);
-analytics('Main.ts!');
-//# sourceMappingURL=main.js.map
--- src/main.ts
+++
@@ -1,6 +0,0 @@
-import { analytics } from './modules/analytics.js';
-
-const message = 'Hello mode';
-console.log(message);
-
-analytics('Main.ts!');
