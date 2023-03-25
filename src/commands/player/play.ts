233d8c0fb5958191129ca10713d3eca397f5cdfa
Ivan Vostrikov <vakann20@gmail.com>
2023-03-25 14:38:40 +0700

    play slash command is ready

--- src/commands/player/play.ts
+++ src/commands/player/play.ts
@@ -1,5 +1,6 @@
-import { SlashCommandBuilder, Client, ChatInputCommandInteraction, Guild, GuildMember } from 'discord.js';
+import { SlashCommandBuilder, Client, ChatInputCommandInteraction, Guild, GuildMember, EmbedBuilder } from 'discord.js';
 import { ICommand } from '../../interfaces/ICommand.js';
+import { QueryType } from 'discord-player';
 
 const playCommand: ICommand = {
   data: new SlashCommandBuilder()
@@ -38,7 +39,71 @@ const playCommand: ICommand = {
     if (!member.voice.channel)
       return interaction.reply({ content: 'You need to be in voice channel to use this command!' });
 
-    return interaction.reply({ content: 'Zaebok' });
+    const queue = await client.player.nodes.create(guild, {
+      volume: 50,
+      leaveOnEmpty: true,
+      leaveOnEnd: true,
+      leaveOnStop: true,
+    });
+    if (!queue.connection) await queue.connect(member.voice.channel);
+
+    const response = new EmbedBuilder();
+    const subCommand: string = interaction.options.getSubcommand();
+
+    if (subCommand === 'song') {
+      const url: string | null = interaction.options.getString('url');
+      if (!url) return interaction.reply({ content: 'Where is url?' });
+
+      const result = await client.player.search(url, {
+        requestedBy: interaction.user,
+        searchEngine: QueryType.YOUTUBE_VIDEO,
+      });
+      if (result.tracks.length === 0) return interaction.reply({ content: 'No results!' });
+
+      const song = result.tracks[0];
+      await queue.addTrack(song);
+
+      response
+        .setDescription(`[${song.title}] has been added to queue!`)
+        .setThumbnail(song.thumbnail)
+        .setFooter({ text: `Duration ${song.duration}` });
+    } else if (subCommand === 'playlist') {
+      const url: string | null = interaction.options.getString('url');
+      if (!url) return interaction.reply({ content: 'Where is url?' });
+
+      const result = await client.player.search(url, {
+        requestedBy: interaction.user,
+        searchEngine: QueryType.YOUTUBE_PLAYLIST,
+      });
+      if (result.tracks.length === 0) return interaction.reply({ content: 'No results!' });
+
+      const playlist = result.playlist;
+      if (!playlist) return interaction.reply({ content: 'There is no playlist!' });
+      await queue.addTrack(result.tracks);
+
+      response.setDescription(`[${playlist.title}] has been added to queue!`);
+    } else if (subCommand === 'search') {
+      const url: string | null = interaction.options.getString('searchterms');
+      if (!url) return interaction.reply({ content: 'There is no Keywords!' });
+
+      const result = await client.player.search(url, {
+        requestedBy: interaction.user,
+        searchEngine: QueryType.AUTO,
+      });
+      if (result.tracks.length === 0) return interaction.reply({ content: 'No results!' });
+
+      const song = result.tracks[0];
+      await queue.addTrack(song);
+
+      response
+        .setDescription(`[${song.title}] has been added to queue!`)
+        .setThumbnail(song.thumbnail)
+        .setFooter({ text: `Duration ${song.duration}` });
+    }
+
+    await interaction.reply({ embeds: [response] });
+
+    if (!queue.isPlaying()) await queue.node.play();
   },
 };
 
